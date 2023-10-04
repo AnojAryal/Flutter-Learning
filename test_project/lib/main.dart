015b276f1a39150c89485bea012b215fffb4f4d3
anojaryal <anoj1810@gmail.com>
2023-10-04 21:35:29 +0545

    building stateful widgets

--- test_project/lib/main.dart
+++ test_project/lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/start_screen.dart';
+import 'package:test_project/quiz.dart';
 
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration:const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen([
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ]),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
