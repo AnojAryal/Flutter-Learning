a3dedbb9ece76fddea3a9e43895c266065cb1927
anojaryal <anoj1810@gmail.com>
2023-10-06 22:26:13 +0545

    styling button

--- test_project/lib/answer_button.dart
+++ test_project/lib/answer_button.dart
@@ -21,7 +21,10 @@ class AnswerButtons extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(40),
           )),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
