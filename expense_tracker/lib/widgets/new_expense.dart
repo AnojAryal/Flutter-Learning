afd57d22552309393911ab1e52df20ebdff21a16
anojaryal <anoj1810@gmail.com>
2023-10-14 23:07:47 +0545

    letting flutter do the work with TextEditingController

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -10,10 +10,18 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enetredTitle = '';
+  //var _enetredTitle = '';
 
-  void _saveTitleInput(String inputValue) {
-    _enetredTitle = inputValue;
+  //void _saveTitleInput(String inputValue) {
+  //  _enetredTitle = inputValue;
+  //}
+  
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,7 +31,8 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
+            //onChanged: _saveTitleInput,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title'),
@@ -33,7 +42,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enetredTitle);
+                  print(_titleController.text);
                 },
                 child: const Text('Save Expense'),
               )
